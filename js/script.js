a963c13e24a268b701ac18ec12fc707c5b7990ab
Aleksei Budilovich <65903462+kybik44@users.noreply.github.com>
2021-08-05 14:24:50 +0300

    Add project

---
+++ js/script.js
@@ -0,0 +1,22 @@
+$("#head__slider").on("click","a", function (event) {
+    event.preventDefault();
+    var id  = $(this).attr('href'),
+        top = $(id).offset().top;
+    $('body,html').animate({scrollTop: top}, 1500);
+});
+$("#head__scroll").on("click","a", function (event) {
+    event.preventDefault();
+    var id  = $(this).attr('href'),
+        top = $(id).offset().top;
+    $('body,html').animate({scrollTop: top}, 1500);
+});
+
+$( ".slider__item" ).hover(
+    function() {
+       
+            $( this ).toggleClass( "slider__item-start" );
+       
+      
+    }
+    
+  );
